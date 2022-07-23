6449ee67914d824f244194006296e80c4918efc8
mudd258 <81851831+mudd258@users.noreply.github.com>
2022-07-23 09:17:33 +0800

    Initial Commit

---
+++ boinkboinkdoink/boinkboinkdoinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  boinkboinkdoinkApp.swift
+//  boinkboinkdoink
+//
+//  Created by Seth Loh on 23/7/22.
+//
+
+import SwiftUI
+
+@main
+struct boinkboinkdoinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
